bb2429041eb0232d03411c8c1eff321cd17bcaa2
LabyKreative <labykreative@yahoo.com>
2023-05-13 12:46:05 -0700

    Store first object

---
+++ models/engine/file_storage.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""Defines the FileStorage class."""
+import json
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
+
+
+class FileStorage:
+    """Represents a file-based storage engine.
+
+    Attributes:
+    __file_path (str): The path to the file where objects are stored.
+    __objects (dict): A dictionary of instantiated objects.
+    """
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """Retrieve all objects.
+
+        Returns:
+            dict: A dictionary of all objects.
+        """
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """Add a new object to the storage.
+
+        Args:
+            obj (BaseModel): The object to be added.
+        """
+        obj_key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        FileStorage.__objects[obj_key] = obj
+
+    def save(self):
+        """Save the objects to the file in JSON format."""
+        obj_dict = {
+                obj_key: obj.to_dict() for obj_key, obj in FileStorage.__objects.items()
+        }
+        with open(FileStorage.__file_path, "w") as file:
+            json.dump(obj_dict, file)
+
+    def reload(self):
+        """Reload objects from the file, if it exists."""
+        try:
+            with open(FileStorage.__file_path, "r") as file:
+                obj_dict = json.load(file)
+                for obj_key, obj_data in obj_dict.items():
+                    class_name = obj_data["__class__"]
+                    del obj_data["__class__"]
+                    self.new(eval(class_name)(**obj_data))
+        except FileNotFoundError:
+            return
