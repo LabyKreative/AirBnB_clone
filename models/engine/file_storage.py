912d9563d7a61630eb9099e72c9cd552fde9a799
LabyKreative <labykreative@yahoo.com>
2023-05-15 03:48:34 -0700

    Fix refectored store first object

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -35,22 +35,22 @@ class FileStorage:
             obj (BaseModel): The object to be added.
         """
         obj_key = obj.__class__.__name__
-        FileStorage.__objects["{}.{}".format(ocname, obj.id)] = obj
+        FileStorage.__objects["{}.{}".format(obj_key, obj.id)] = obj
 
     def save(self):
         """Save the objects to the file in JSON format."""
 
         obj_dict = FileStorage.__objects
-        obj_dict = {obj: obj_dict[obj].to_dict() for obj in obj_dict.keys()}
+        obj_dic = {obj: obj_dict[obj].to_dict() for obj in obj_dict.keys()}
         with open(FileStorage.__file_path, "w") as f:
-            json.dump(obj_dict, f)
+            json.dump(obj_dic, f)
 
     def reload(self):
         """Reload objects from the file, if it exists."""
         try:
             with open(FileStorage.__file_path) as f:
-                obj_dict = json.load(f)
-                for obj_data in obj_dict.values():
+                obj_dic = json.load(f)
+                for obj_data in obj_dic.values():
                     class_name = obj_data["__class__"]
                     del obj_data["__class__"]
                     self.new(eval(class_name)(**obj_data))
