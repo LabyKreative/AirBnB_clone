525269d3bf343bb0a8fe0dda22319e49af0f6c9c
LabyKreative <labykreative@yahoo.com>
2023-05-15 03:39:23 -0700

    Refactored store first object

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -34,24 +34,23 @@ class FileStorage:
         Args:
             obj (BaseModel): The object to be added.
         """
-        obj_key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        FileStorage.__objects[obj_key] = obj
+        obj_key = obj.__class__.__name__
+        FileStorage.__objects["{}.{}".format(ocname, obj.id)] = obj
 
     def save(self):
         """Save the objects to the file in JSON format."""
-        obj_dict = {
-            obj_key: obj.to_dict()
-            for obj_key, obj in FileStorage.__objects.items()
-        }
-        with open(FileStorage.__file_path, "w") as file:
-            json.dump(obj_dict, file)
+
+        obj_dict = FileStorage.__objects
+        obj_dict = {obj: obj_dict[obj].to_dict() for obj in obj_dict.keys()}
+        with open(FileStorage.__file_path, "w") as f:
+            json.dump(obj_dict, f)
 
     def reload(self):
         """Reload objects from the file, if it exists."""
         try:
-            with open(FileStorage.__file_path, "r") as file:
-                obj_dict = json.load(file)
-                for obj_key, obj_data in obj_dict.items():
+            with open(FileStorage.__file_path) as f:
+                obj_dict = json.load(f)
+                for obj_data in obj_dict.values():
                     class_name = obj_data["__class__"]
                     del obj_data["__class__"]
                     self.new(eval(class_name)(**obj_data))
